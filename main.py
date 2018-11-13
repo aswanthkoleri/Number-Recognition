d4a4e6d7f08644cca85d1b69b5e626d7928c97b1
aswanthkoleri <aswanth9495@gmail.com>
2018-11-13 13:44:44 +0530

    Add input option

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ model = load_model('models/mnistCNN.h5')
 from PIL import Image
 import numpy as np
 
-for index in range(10):
+def main():
+    print("Enter the image file name : ")
+    index=input()
     img = Image.open('data/' + str(index) + '.png').convert("L")
     img = img.resize((28,28))
     im2arr = np.array(img)
     im2arr = im2arr.reshape(1,28,28,1)
     # Predicting the Test set results
     y_pred = model.predict(im2arr)
-    print(y_pred)
+    # print(y_pred)
+    answer=y_pred[0]
+    for i in range(len(answer)):
+        if(answer[i]==1):
+            print(i)
+main()
